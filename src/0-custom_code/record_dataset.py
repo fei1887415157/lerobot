2e148a18ce5b7a936c8b249b9d4df7263211643a
Felix-Jetson <fei1887415157@gmail.com>
2026-01-22 17:31:24 -0800

    config; create vs append init

--- src/0-custom_code/record_dataset.py
+++ src/0-custom_code/record_dataset.py
@@ -1,34 +1,39 @@
 from lerobot.cameras.opencv.configuration_opencv import OpenCVCameraConfig
 from lerobot.datasets.lerobot_dataset import LeRobotDataset
 from lerobot.datasets.utils import hw_to_dataset_features
-from lerobot.robots.so_follower import SO101Follower, SO101FollowerConfig
-from lerobot.teleoperators.so_leader.config_so100_leader import SO101LeaderConfig
-from lerobot.teleoperators.so_leader.so100_leader import SO101Leader
+from lerobot.robots.so101_follower import SO101Follower, SO101FollowerConfig
+from lerobot.teleoperators.so101_leader.config_so101_leader import SO101LeaderConfig
+from lerobot.teleoperators.so101_leader.so101_leader import SO101Leader
 from lerobot.utils.control_utils import init_keyboard_listener
 from lerobot.utils.utils import log_say
 from lerobot.utils.visualization_utils import init_rerun
 from lerobot.scripts.lerobot_record import record_loop
 from lerobot.processor import make_default_processors
+import os
 
-NUM_EPISODES = 5
+
+
+TASK_DESCRIPTION = "test task"
+NUM_EPISODES = 3
+EPISODE_TIME_SEC = 5
+RESET_TIME_SEC = 5
 FPS = 30
-EPISODE_TIME_SEC = 60
-RESET_TIME_SEC = 10
-TASK_DESCRIPTION = "My task description"
-
-# Create robot configuration
-robot_config = SO101FollowerConfig(
-    id="my_awesome_follower_arm",
-    cameras={
-        "front": OpenCVCameraConfig(index_or_path=0, width=640, height=480, fps=FPS) # Optional: fourcc="MJPG" for troubleshooting OpenCV async error.
-    },
-    port="/dev/tty.usbmodem58760434471",
-)
 
 teleop_config = SO101LeaderConfig(
-    id="my_awesome_leader_arm",
-    port="/dev/tty.usbmodem585A0077581",
+    id="leader",
+    port="/dev/ttyACM0",
 )
+
+robot_config = SO101FollowerConfig(
+    id="follower",
+    port="/dev/ttyACM1",
+    cameras={
+        "0": OpenCVCameraConfig(index_or_path=0, width=640, height=360, fps=FPS, fourcc='MJPG'),
+        "2": OpenCVCameraConfig(index_or_path=2, width=640, height=360, fps=FPS, fourcc='MJPG')
+    }
+)
+
+
 
 # Initialize the robot and teleoperator
 robot = SO101Follower(robot_config)
@@ -39,15 +44,23 @@ action_features = hw_to_dataset_features(robot.action_features, "action")
 obs_features = hw_to_dataset_features(robot.observation_features, "observation")
 dataset_features = {**action_features, **obs_features}
 
-# Create the dataset
-dataset = LeRobotDataset.create(
-    repo_id="<hf_username>/<dataset_repo_id>",
-    fps=FPS,
-    features=dataset_features,
-    robot_type=robot.name,
-    use_videos=True,
-    image_writer_threads=4,
-)
+repo_id = "fei1887415157/test_dataset"
+local_dir = "/home/robobond/.cache/huggingface/lerobot/fei1887415157/test_dataset"
+
+if os.path.exists(local_dir):
+    # LOAD: This keeps existing episodes and appends new ones
+    dataset = LeRobotDataset(repo_id)
+    print("Existing dataset found. Appending new episodes.")
+else:
+    # CREATE: Only runs the very first time
+    dataset = LeRobotDataset.create(
+        repo_id=repo_id,
+        fps=FPS,
+        features=dataset_features,
+        robot_type=robot.name,
+        use_videos=True
+    )
+    print("Created a brand new dataset.")
 
 # Initialize the keyboard listener and rerun visualization
 _, events = init_keyboard_listener()
@@ -103,6 +116,8 @@ while episode_idx < NUM_EPISODES and not events["stop_recording"]:
 
     dataset.save_episode()
     episode_idx += 1
+
+
 
 # Clean up
 log_say("Stop recording")
